7150c61617bb474bdd357a1cde2dacd4f147687a
po <30716611+sinian0926@users.noreply.github.com>
2019-01-07 23:31:59 +0800

    Update main.py

--- spider_dytt/main.py
+++ spider_dytt/main.py
@@ -1,11 +1,9 @@
 '''
    @Author Ci Xin
    @E-mail sssrrr879@126.com
-   @Phone 15849311404
    @Desc 
    @Date Create on 2018/12/8/008 12:08
 '''
 from scrapy import cmdline
 
 cmdline.execute('scrapy crawl dytt_spider'.split())
-#download 没有做同步
